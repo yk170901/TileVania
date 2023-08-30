33ec07fb9b22190908c49a308581159038ce4a4b
yk170901 <yk170901@gmail.com>
2023-08-30 20:45:06 +0900

    Move character according to user input

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Vector2 moveInput;
+    Rigidbody2D myRigidBody;
+    CapsuleCollider2D myCollider;
+    
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _jumpSpeed;
+    Animator myAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCollider = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
+        FlipSprite();
+    }
+
+    private void FlipSprite()
+    {
+        bool isHorizontallyMoving = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+        if (isHorizontallyMoving)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+    }
+
+    private void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * _moveSpeed, myRigidBody.velocity.y); // y = 0 so that the player cannot fly
+        myRigidBody.velocity = playerVelocity;
+
+        myAnimator.SetBool("isRunning", Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon);
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if(value.isPressed && myCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            myRigidBody.velocity += new Vector2(0f, _jumpSpeed);
+        }
     }
 }
